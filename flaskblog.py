5087b056cf9689208dff859ec57f969699f41099
Ron Almog <ron8132@gmail.com>
2019-09-09 06:27:59 -0400

    login, register templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '123123'
 posts = [
   {
     'author': 'Johny Smith',
@@ -21,6 +24,16 @@ posts = [
 @app.route("/")
 def home():
     return render_template('home.html', posts=posts)
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)  
 
 @app.route("/about")
 def about():  
