673b29c4187f53adaac2e5f429f81b5d4d3ff4c1
Ron Almog <ron8132@gmail.com>
2019-09-09 15:23:15 -0400

    form validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -25,9 +25,12 @@ posts = [
 def home():
     return render_template('home.html', posts=posts)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))      
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
