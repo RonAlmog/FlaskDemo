5834239567e91c311c28b9b78346891ec46a154e
Ron Almog <ron8132@gmail.com>
2019-09-10 02:13:01 -0400

    login works

--- flaskblog.py
+++ flaskblog.py
@@ -33,9 +33,15 @@ def register():
         return redirect(url_for('home'))      
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+      if form.email.data == 'admin@blog.com' and form.password.data == '123':
+        flash('You have logged in!', 'success')
+        return redirect(url_for('home'))
+      else:
+        flash('Login unsuccessful. Please check your user name and password.', 'danger')
     return render_template('login.html', title='Login', form=form)  
 
 @app.route("/about")
